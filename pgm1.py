1be583614fd418ab2b0d2f54708a62ccd86e55fc
mahi <maheswarishanthi29@gmail.com>
2021-05-10 17:40:55 +0100

    Initial import

---
+++ pgm1.py
@@ -0,0 +1,7 @@
+def remove_duplicates(lista):
+    lista2 =[]
+    for item in lista:
+         if item not in lista2:
+             lista2.append(item)
+    return lista2 
+print(remove_duplicates([1,2,2,3,3]))  
